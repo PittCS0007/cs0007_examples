ac52cbf37f30afdad94ffc0b82cabd8855611624
Nick Farnan <nlf4@pitt.edu>
2024-05-22 17:11:37 -0400

    Adding more code from lecture on 05/22

---
+++ decision_structures/BooleTests.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BooleTests {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter an int: ");
+    int userNumber = input.nextInt();
+
+    if (userNumber < 10) {
+      System.out.println("You entered a number less than 10!");
+    }
+    else if (userNumber > 10) {
+      if (userNumber > 5) {
+        System.out.println("You entered a number MUCH greater than 10");
+      }
+      else {
+        System.out.println("You entered a number greater than 10!");
+      }
+    }
+    else {
+      System.out.println("You entered 10!");
+    }
+    System.out.println("This line is printed in all cases");
+  }
+}
